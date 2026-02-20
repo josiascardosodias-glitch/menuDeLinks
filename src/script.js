741a48cb2f98aeca413af10724972e1da702ceeb
josiascardosodias-glitch <josiascardosodias@gmail.com>
2026-02-19 23:59:38 -0300

    primeiro push

---
+++ src/script.js
@@ -0,0 +1,15 @@
+const goWhats = document.getElementById('goWhats');
+imgButton.addEventListener("click", () => {
+    popup.classList.toggle("active");
+
+    if (popup.classList.contains("active")) {
+        imgButton.src = "../src/styles/assets/images/hotbar-right.png";
+    } else {
+        imgButton.src = "../src/styles/assets/images/hotbar-left.png";
+    }
+});
+
+goWhats.addEventListener("click", () => {
+    const mensagem = encodeURIComponent("Olá! Gostaria de saber um pouco mais sobre o seu trabalho.");
+    window.open(`https://wa.me/5551989622580?text=${mensagem}`, "_blank");
+});
