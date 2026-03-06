6dcd043685901fde52e54d140ecb0a5813f51ba8
josiascardosodias-glitch <josiascardosodias@gmail.com>
2026-03-06 15:56:02 -0300

    eventos com touch

--- script.js
+++ script.js
@@ -13,3 +13,14 @@ goWhats.addEventListener("click", () => {
     const mensagem = encodeURIComponent("Olá! Gostaria de saber um pouco mais sobre o seu trabalho.");
     window.open(`https://wa.me/5551989622580?text=${mensagem}`, "_blank");
 });
+
+const buttons = document.querySelectorAll('.button');
+
+buttons.forEach((btn) => {
+    btn.addEventListener("pointerdown", () => {
+        document.body.classList.add("activeBnt");
+    });
+    btn.addEventListener("pointerup", () => {
+        document.body.classList.remove("activeBnt");
+    });
+});
